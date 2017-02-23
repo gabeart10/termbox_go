aa3e42ed2c1cd20567c15329f24723927f5e4774
Gabe Miller <millergabe10@gmail.com>
2017-02-22 19:22:04 -0500

    save

--- rainbow_term/main.go
+++ rainbow_term/main.go
@@ -1,18 +1,33 @@
 package main
 import (
-	"github.com/nsf/termbox-go" t
+	t "github.com/nsf/termbox-go"
+	"os"
 )
 
 func main() {
 	t.SetOutputMode(t.Output256)
-	var colors [6]int{1,202,11,10,19,17,18}
+	var colors [7]int{1,202,11,10,19,17,18}
 	var current_color = 0
 	go func() {
 		for {
 			var exitEvent = t.PollEvent()
 			if exitEvent.Key == t.KeyCtrlC {
 				t.Close()
-				os.Exit(3) for {
+				os.Exit(3) 
+			}
+		}
+	}()
+	t.Init()
+	for {
 		var w, h = t.Size()
-		for i := 0;i < w;i++ {
-			
+		for i := 0;i < h;i++ {
+			for n := 0;n < w;n++ {
+				t.SetCell(i,n," ",0,colors[current_color])
+				if current_color == 6 {
+					current_color = 0
+				}
+				current_color++
+			}
+		}
+	}
+}
